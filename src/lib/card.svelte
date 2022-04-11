454625611ec263cb5ca34f96d8e5c6cf8d6ae0da
Darren Potter <darpots@outlook.com>
2022-04-11 13:18:06 +0100

    first commit

---
+++ src/lib/card.svelte
@@ -0,0 +1,93 @@
+<script>
+	export let image;
+	export let alt;
+	export let heading;
+	export let text;
+	export let buttonText;
+	export let bgcolor;
+</script>
+
+<section style="--bg: var(--{bgcolor})">
+	<div class="container">
+		<div class="image"><img src={image} {alt} /></div>
+		<h1>{heading}</h1>
+		<p>{text}</p>
+		<div>
+			<button style="--bg: var(--{bgcolor})">
+				{buttonText}
+			</button>
+		</div>
+	</div>
+</section>
+
+<style>
+	section {
+		background-color: var(--bg);
+		padding: 3rem;
+	}
+
+	.container {
+		max-width: 14.4375rem;
+		margin: 0 auto;
+	}
+	.image {
+		margin-bottom: 2.1875rem;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		color: var(--gray);
+		font-family: 'Big Shoulders Display', cursive;
+		font-weight: 700;
+		font-size: 2.5rem;
+		line-height: 3rem;
+		margin-bottom: 1.5625rem;
+	}
+	p {
+		font-family: 'Lexend Deca', sans-serif;
+		font-size: 0.9375rem;
+		line-height: 1.5625rem;
+		color: var(--white);
+		mix-blend-mode: normal;
+		opacity: 0.75;
+		margin-bottom: 1.5625rem;
+	}
+	button {
+		border-radius: 1.5625rem;
+		background-color: var(--gray);
+		font-family: 'Lexend Deca';
+		font-size: 0.9375rem;
+		line-height: 1.5625rem;
+		border: 0;
+		color: var(--bg);
+		padding: 0.625rem 1.875rem;
+		text-transform: capitalize;
+		border: 0.125rem var(--gray) solid;
+	}
+	button:hover {
+		background-color: transparent;
+		color: var(--gray);
+		border: 0.125rem var(--gray) solid;
+	}
+
+	@media screen and (min-width: 48.0625rem) {
+		.container {
+			max-width: 13.25rem;
+		}
+		.image {
+			margin-bottom: 2.1875rem;
+		}
+		h1 {
+			margin-bottom: 1.5625rem;
+		}
+		p {
+			margin-bottom: 5.1875rem;
+		}
+		section:first-of-type {
+			border-radius: 0.5rem 0 0 0.5rem;
+		}
+		section:last-of-type {
+			border-radius: 0 0.5rem 0.5rem 0;
+		}
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,49 @@
+<script>
+	import '$lib/globalStyles.css';
+	import Card from '$lib/card.svelte';
+</script>
+
+<svelte:head>
+	<title>Preview Card</title>
+</svelte:head>
+
+<div class="container">
+	<Card
+		image="/icon-sedans.svg"
+		alt="sedan"
+		heading="sedans"
+		text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+		buttonText="learn more"
+		bgcolor="orange"
+	/>
+	<Card
+		image="/icon-suvs.svg"
+		alt="suv"
+		heading="suvs"
+		text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+		buttonText="learn more"
+		bgcolor="cyan"
+	/>
+	<Card
+		image="/icon-luxury.svg"
+		alt="luxury"
+		heading="luxury"
+		text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. "
+		buttonText="learn more"
+		bgcolor="darkCyan"
+	/>
+</div>
+
+<style>
+	.container {
+		background-color: var(--gray);
+	}
+	@media screen and (min-width: 48.0625rem) {
+		.container {
+			display: grid;
+			place-content: center;
+			grid-auto-flow: column;
+			height: 100vh;
+		}
+	}
+</style>
